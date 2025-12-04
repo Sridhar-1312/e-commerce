9e02c629aeb2d1e99373daa7dc6ededcbb9b2e4b
Sridhar-1312 <ronaldosriiicr7@gmail.com>
2025-12-04 21:40:09 +0530

    modified admin.css

---
+++ src/Components/ListProduct/Listproduct.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Listproduct.css'
+const Listproduct = () => {
+  return (
+    <div className='list-product'>Listproduct</div>
+  )
+}
+
+export default Listproduct
