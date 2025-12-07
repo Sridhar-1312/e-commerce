8268b9975e392c4180b5d108395d3f46344a606b
Sridhar-1312 <ronaldosriiicr7@gmail.com>
2025-12-07 21:38:42 +0530

    modified admin.css

--- src/Components/AddProduct/AddProduct.jsx
+++ src/Components/AddProduct/AddProduct.jsx
@@ -8,6 +8,16 @@ const AddProduct = () => {
             <p>Product Title</p>
             <input type="text"  name='name' placeholder='Type here'/>
         </div>
+        <div className="addproduct-price">
+          <div className="addproduct-itemfield">
+            <p>Price</p>
+            <input type="text" name="old_price" placeholder="Type here" />
+          </div>
+          <div className="addproduct-itemfield">
+            <p>Offer Price</p>
+            <input type="text" name="new_price" placeholder="Type here" />
+          </div>
+        </div>
     </div>
   )
 }
